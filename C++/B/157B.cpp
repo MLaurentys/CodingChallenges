afd083e9d1c5f87795a087a7a83a134ad3fd0aa3
MLaurentys <matheus_t.laurentys@hotmail.com>
2016-09-27 19:39:09 -0400

    Add files via upload

---
+++ C++/B/157B.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+#include<algorithm>//for c++ sort
+#include<vector>//for vector
+#define PI 3.141592653589793238462643383279502884L //pi
+using namespace std;
+int main()
+{
+	int amountOfCircles;
+	cin >> amountOfCircles;
+	vector<int> radiusList;
+	int currentInput = 0;
+	unsigned long long int totalArea = 0;
+
+	for (int i = 0; i < amountOfCircles; i++) {
+		cin >> currentInput;
+		radiusList.push_back(currentInput);
+	}
+	sort(radiusList.begin(), radiusList.end());// Ex: 1, 2, 3, 4, ...
+	//it is better to work from the largest to the smaller, because
+	//you cannot tell which collor the first circle is painted
+	reverse(radiusList.begin(), radiusList.end());// Ex 20, 19, 18, 17, ...
+	for (int i = 0; i < amountOfCircles; i++){
+		if (i % 2 == 0)
+			totalArea += radiusList.at(i) * radiusList.at(i);
+		else
+			totalArea -= radiusList.at(i) * radiusList.at(i);
+	}
+	cout << totalArea*PI;
+	return 0;
+}
