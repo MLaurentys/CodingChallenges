ce974b85f78f6f50a4ee3d6b4c7600fbbd8c270b
MLaurentys <matheus_t.laurentys@hotmail.com>
2021-11-16 14:21:01 -0500

    added another solution

---
+++ leetcode/907.py
@@ -0,0 +1,14 @@
+class Solution:
+    def sumSubarrayMins(self, arr: List[int]) -> int:
+        dp = [0] * len(arr)
+        stack = []
+        for ind, el in enumerate(arr):
+            while len(stack) > 0 and stack[-1][0] > el:
+                stack.pop()
+            if len(stack) != 0:
+                dp[ind] = dp[stack[-1][1]] + el * (ind - stack[-1][1])
+            else:
+                dp[ind] = (ind + 1) * el
+
+            stack.append((el, ind))
+        return sum(dp) % (10**9 + 7)
